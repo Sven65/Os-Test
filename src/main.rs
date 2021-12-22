313750f7201d64189c44614eb18ea65210ad464f
Sven65 <Sven65@users.noreply.github.com>
2021-12-22 13:32:25 +0100

    Keyboards

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,11 @@ pub extern "C" fn _start() -> ! {
 
     test_os::init();
 
-    fn stack_overflow() {
-        stack_overflow(); // for each recursion, the return address is pushed
-    }
-
-    // trigger a stack overflow
-    stack_overflow();
-
     #[cfg(test)]
     test_main();
 
     println!("No crashes, woo!");
-
-    loop {}
+    test_os::hlt_loop();
 }
 
 /// This function is called on panic.
@@ -33,7 +25,7 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    test_os::hlt_loop();
 }
 
 #[cfg(test)]
