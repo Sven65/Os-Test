b781a07e790fc3a36b418f33c235326a0e6cdf15
Sven65 <Sven65@users.noreply.github.com>
2021-12-23 08:18:22 +0100

    Memory

--- src/main.rs
+++ src/main.rs
@@ -11,36 +11,33 @@ use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
 
 use test_os::memory;
-use x86_64::{structures::paging::Translate, VirtAddr};
+use test_os::memory::BootInfoFrameAllocator;
+use x86_64::{structures::paging::Page, VirtAddr};
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use test_os::memory::active_level_4_table;
-
     println!("Hello World{}", "!");
 
     test_os::init();
 
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let mut mapper = unsafe { memory::init(phys_mem_offset) };
 
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
+    // let mut frame_allocator = unsafe {
+    //     BootInfoFrameAllocator::init(&boot_info.memory_map)
+    // };
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    // // map an unused page
+    // //let page = Page::containing_address(VirtAddr::new(0));
+    // let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+   
+    // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // // write the string `New!` to the screen through the new mapping
+    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+
 
     #[cfg(test)]
     test_main();
