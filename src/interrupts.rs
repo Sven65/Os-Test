5fb10675e4743298e6d5b8476df04b08c00a3012
Sven65 <Sven65@users.noreply.github.com>
2026-06-11 12:27:01 +0200

    chore: nightly-2026-06-01, x86_64 0.15, pic8259 0.11, uart_16550 0.3, drop unused vga crateBasic network stack

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -38,10 +38,10 @@ lazy_static! {
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
 
-        idt[InterruptIndex::Timer.as_usize()]
+        idt[InterruptIndex::Timer.as_u8()]
             .set_handler_fn(timer_interrupt_handler);
 
-        idt[InterruptIndex::Keyboard.as_usize()]
+        idt[InterruptIndex::Keyboard.as_u8()]
             .set_handler_fn(keyboard_interrupt_handler);
 
         idt.page_fault.set_handler_fn(page_fault_handler);
